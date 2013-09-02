1de6524e0fb60b6a99657c6ab5046646ff7a3166
JotaAlava <jalava85@gmail.com>
2013-09-02 12:14:57 -0400

    Small Changes
    Created the partial views to make the main view more manageable. Also
    added a customized HTML helper to stay within ASP.NET Best Practices
    when creating action link buttons.

---
+++ SportsHub/HelperMethods/CustomHtmlHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsHub.HelperMethods
+{    
+    public static class CustomHtmlHelpers
+    {
+        /// <summary>
+        /// This is a custom HTML helper created by Jota to stay within ASP.NET best practices when implementing action links as buttons.
+        /// </summary>
+        /// <param name ="htmlClass">By default this project uses this with Twitter's Bootstratp, and this is intended to select the button type.</param>
+        /// <param name ="action">The action to call in a controller</param>
+        /// <param name ="controller">The controller in which the action will be called.</param>
+        /// <param name ="id">This is an id element that we can use to locate the event in the Db that is going to have the current active player added.</param>
+        /// <param name ="textToDisplay">The text displayed by the action link</param>
+        /// <param name ="elementStatus">This element status controlls whether or not the element is grayed out for a user. This is a default parameter set to Enabled, so if no value is passed an element will be enabled. It expects "Disabled" as a value, other values may produce unexpected behavior.</param>
+        public static MvcHtmlString BootstrapCustomizableButtonActionLink(this HtmlHelper helper, string htmlClass, string action, string controller, int id, string textToDisplay, string elementStatus = "Enabled")
+        {
+            return new MvcHtmlString("<a  class=\" " + htmlClass + "\" href=\"/" + controller + "/" + action + "/" + id + "\"" + elementStatus + "> " + textToDisplay + "</a>");
+        }
+    }
+}
