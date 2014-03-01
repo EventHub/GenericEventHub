3657d8aefb78e9e433f44169d8e1824628693c36
Kevin Pike <kevin.e.pike@gmail.com>
2014-03-01 08:41:17 -0500

    generic and base repository

---
+++ GenericEventHub/GenericEventHub/Repositories/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GenericEventHub.Repositories
+{
+    public class BaseRepository<TEntity> where TEntity : class
+    {
+        protected IGenericRepository<TEntity> _repo;
+
+        public BaseRepository(IGenericRepository<TEntity> repo)
+        {
+            _repo = repo;
+        }
+
+        public void Insert(TEntity entity)
+        {
+            _repo.Insert(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _repo.Update(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _repo.Delete(entity);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _repo.Get();
+        }
+
+        public TEntity GetByID(object id)
+        {
+            return _repo.GetByID(id);
+        }
+
+
+        public void Dispose()
+        {
+            _repo.Dispose();
+        }
+
+    }
+
+    public interface IBaseRepository<TEntity> : IDisposable where TEntity : class
+    {
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        TEntity GetByID(object id);
+        IEnumerable<TEntity> GetAll();
+    }
+}
---
+++ GenericEventHub/GenericEventHub/Repositories/GenericRepository.cs
@@ -0,0 +1,99 @@
+﻿using GenericEventHub.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace GenericEventHub.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        internal GenericEventHubDb context;
+        internal DbSet<TEntity> dbSet;
+
+        public GenericRepository(GenericEventHubDb context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public virtual TEntity GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+            context.SaveChanges();
+        }
+
+        public virtual void Delete(object id)
+        {
+            TEntity entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+            context.SaveChanges();
+        }
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+
+    public interface IGenericRepository<TEntity> : IDisposable
+     where TEntity : class
+    {
+        void Delete(object id);
+        void Delete(TEntity entityToDelete);
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+        TEntity GetByID(object id);
+        void Insert(TEntity entity);
+        void Update(TEntity entityToUpdate);
+    }
+}
